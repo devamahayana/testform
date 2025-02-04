ec94e034aea6c190b1f1d3363789665ceeb813d2
Deva Mahayana <devamahayanatop@gmail.com>
2025-02-04 15:02:44 +0800

    sdds

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true, // Keep strict mode enabled
-  experimental: {}, // Ensure there are no unnecessary keys
-  output: 'export',
-};
-
-export default nextConfig;
